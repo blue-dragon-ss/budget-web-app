d661a440394ec25b717fdc135223781f6c7fa311
blue-dragon-ss <236330799+blue-dragon-ss@users.noreply.github.com>
2025-10-19 11:24:58 +0900

    HTTPS疎通・環境変数外出し対応

---
+++ scripts/run-https.ps1
@@ -0,0 +1,13 @@
+# ① DBを起動（1つ上の階層の docker-compose.yml を指定）
+docker compose -f ../docker-compose.yml up -d
+
+# ② 1つ上の階層の .env を読み込んで環境変数に設定
+Get-Content ../.env | ForEach-Object {
+  if ($_ -match "^(?<key>[^=]+)=(?<value>.+)$") {
+    [System.Environment]::SetEnvironmentVariable($matches['key'], $matches['value'])
+  }
+}
+
+# ③ Gradleを実行（プロジェクトディレクトリを..に明示）
+#   ※ -p .. もしくは --project-dir .. を必ず付ける
+..\gradlew -p .. bootRun --args='--spring.profiles.active=https'
