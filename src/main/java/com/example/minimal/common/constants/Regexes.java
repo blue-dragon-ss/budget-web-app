b83eafdbf734928ed82d00cdf3b2af9254e10fc1
blue-dragon-ss <236330799+blue-dragon-ss@users.noreply.github.com>
2025-10-27 21:59:31 +0900

    R:正規化パターンの修正

--- src/main/java/com/example/minimal/common/constants/Regexes.java
+++ src/main/java/com/example/minimal/common/constants/Regexes.java
@@ -3,6 +3,6 @@ package com.example.minimal.common.constants;
 public final class Regexes {
     private Regexes() {}
 
-    public static final String EMAIL = "^(|[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,})$";
+    public static final String EMAIL = "^(|[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,})$";
     public static final String ULID  = "^[0-9A-HJKMNP-TV-Z]{26}$";
 }
