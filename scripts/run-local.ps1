2f3bd9b2f03f127375bce8332bec902b0de29654
blue-dragon-ss <236330799+blue-dragon-ss@users.noreply.github.com>
2026-01-29 11:53:40 +0900

    READMEとビルド整備 (#37)
    * 一次開発：開発ビルドスクリプト修正
    
    * READMEとgradle-wrapperを追加
    
    * READMEをGithub上で見えやすいように修正
    
    Update README to enhance clarity and structure

--- scripts/run-local.ps1
+++ scripts/run-local.ps1
@@ -1,12 +1,15 @@
-# ① DBを起動（1つ上の階層の docker-compose.yml を指定）
-docker compose -f ../docker-compose.yml up -d
+# ※ PowerShellのエンコードをUTF8にする（必要なければ外す）※
+$OutputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-# ② 1つ上の階層の .env を読み込んで環境変数に設定
+# ① 1つ上の階層の .env を読み込んで環境変数に設定
 Get-Content ../.env | ForEach-Object {
   if ($_ -match "^(?<key>[^=]+)=(?<value>.+)$") {
     [System.Environment]::SetEnvironmentVariable($matches['key'], $matches['value'])
   }
 }
+
+# ② DBを起動（1つ上の階層の docker-compose.yml を指定）
+docker compose -f ../docker-compose.yml up -d
 
 # ③ Gradleを実行（プロジェクトディレクトリを..に明示）
 #   ※ -p .. もしくは --project-dir .. を必ず付ける
